87bbbe09691a3b4417c103f5db9e5d73583d9b1f
Andrew Melad <andrewmelad108@gmail.com>
2024-09-29 15:44:42 +0300

    change paths

--- store/aroa-store.ts
+++ store/aroa-store.ts
@@ -2,6 +2,6 @@ import { create } from "zustand";
 
 export const useAppStore = create((set) => ({
   counter: 0,
-  increase: () => set((state) => ({ counter: state.counter + 1 })),
+  increase: () => set((state: any) => ({ counter: state.counter + 1 })),
   removeAllCounter: () => set({ counter: 0 }),
 }));
